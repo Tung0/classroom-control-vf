a61326dc7030b22236df77a057d85e91164b93a2
Tung0 <taylor19059@gmail.com>
2016-07-21 15:01:41 -0700

    Create managed_user.pp

---
+++ site/users/manifests/managed_user.pp
@@ -0,0 +1,13 @@
+define users::managed_user(
+  $group = $title
+  ){
+    user { $title:
+      ensure => present,
+    }
+    file { /home/$title:
+      ensure => directory,
+      group  => $group,
+      user   => $title,
+    }
+}
+      
