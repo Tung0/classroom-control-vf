11119df879a0774e8e4b0dd9723fa43714356376
Tung0 <taylor19059@gmail.com>
2016-07-21 10:22:59 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,34 +1,42 @@
 class nginx {
+  
   File {
     ensure => file,
     owner  => 'root',
     group  => 'root',
     mode   => '0664',
   }
+  
   package {'nginx':
      ensure => present,
   }
+  
   file {'/var/www':
     ensure => directory,
     mode   => '0775',
   }  
+  
   file {'/var/www/index.html':
     source => 'puppet:///modules/nginx/index.html',  
   }  
+  
   file {'/etc/nginx/nginx.conf':
     source  => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
     notify  => Service['nginx'],  
   }  
+  
   file {'/etc/nginx/conf.d':
     ensure => directory,
     mode   => '0775',
   }  
+  
   file {'/etc/nginx/conf.d/default.conf':
     source  => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
     notify  => Service['nginx'],  
   }  
+  
   service {'nginx':
     ensure  => running,
     enable  => true,
